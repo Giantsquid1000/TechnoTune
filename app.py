b099f6e51798dbcb56c3f6a816cd7e9287f2a7ab
Aaryan <aaryan@Aaryans-MacBook-Air.local>
2024-07-31 13:50:26 -0400

    changes

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(debug=True)
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html')
 
 @app.route('/home')
-def hello_world():
+def hello_home():
     return 'Hello, Home!'
